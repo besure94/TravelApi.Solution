2b4b418fee7f560db444023836c6c33af4c99b79
Brian Scherner <brianscherner@gmail.com>
2023-12-21 11:31:16 -0800

    Seed dummy data to database and update DB.

---
+++ TravelApi/Migrations/20231221192825_SeedData.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TravelDestinations",
+                columns: new[] { "TravelDestinationId", "City", "Country", "NumberOfReviews", "OverallRating", "Reviews" },
+                values: new object[,]
+                {
+                    { 1, "Dublin", "Ireland", 234, 10, "Beautiful and friendly! Will visit again." },
+                    { 2, "St Louis", "United States", 43, 4, "Historic and walkable but very dirty." },
+                    { 3, "Cairo", "Egypt", 435, 7, "Great attractions and history. Crowded and smoggy." },
+                    { 4, "Puerto Vallarta", "Mexico", 128, 9, "Beautiful, relaxing and warm." },
+                    { 5, "Shanghai", "China", 879, 10, "Diverse, lots of entertainment, great food." }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TravelDestinations",
+                keyColumn: "TravelDestinationId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TravelDestinations",
+                keyColumn: "TravelDestinationId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TravelDestinations",
+                keyColumn: "TravelDestinationId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TravelDestinations",
+                keyColumn: "TravelDestinationId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "TravelDestinations",
+                keyColumn: "TravelDestinationId",
+                keyValue: 5);
+        }
+    }
+}
--- TravelApi/Models/TravelApiContext.cs
+++ TravelApi/Models/TravelApiContext.cs
@@ -9,5 +9,17 @@ namespace TravelApi.Models
     {
 
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<TravelDestination>()
+        .HasData(
+          new TravelDestination { TravelDestinationId = 1, City = "Dublin", Country = "Ireland", Reviews = "Beautiful and friendly! Will visit again.", NumberOfReviews = 234, OverallRating = 10 },
+          new TravelDestination { TravelDestinationId = 2, City = "St Louis", Country = "United States", Reviews = "Historic and walkable but very dirty.", NumberOfReviews = 43, OverallRating = 4 },
+          new TravelDestination { TravelDestinationId = 3, City = "Cairo", Country = "Egypt", Reviews = "Great attractions and history. Crowded and smoggy.", NumberOfReviews = 435, OverallRating = 7 },
+          new TravelDestination { TravelDestinationId = 4, City = "Puerto Vallarta", Country = "Mexico", Reviews = "Beautiful, relaxing and warm.", NumberOfReviews = 128, OverallRating = 9 },
+          new TravelDestination { TravelDestinationId = 5, City = "Shanghai", Country = "China", Reviews = "Diverse, lots of entertainment, great food.", NumberOfReviews = 879, OverallRating = 10}
+      );
+    }
   }
 }
