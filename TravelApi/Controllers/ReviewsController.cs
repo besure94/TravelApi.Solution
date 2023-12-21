c197c3ad1e1d2e8c356a9675581da5c506a327f0
Brian Scherner <brianscherner@gmail.com>
2023-12-21 13:17:57 -0800

    Add functionality to get all reviews + review by ID.

--- TravelApi/Controllers/ReviewsController.cs
+++ TravelApi/Controllers/ReviewsController.cs
@@ -6,10 +6,10 @@ namespace TravelApi.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
-  public class TravelDestinationsController : ControllerBase
+  public class ReviewsController : ControllerBase
   {
     private readonly TravelApiContext _db;
-    public TravelDestinationsController(TravelApiContext db)
+    public ReviewsController(TravelApiContext db)
     {
       _db = db;
     }
@@ -18,6 +18,19 @@ namespace TravelApi.Controllers
     public async Task<ActionResult<IEnumerable<Review>>> Get()
     {
       return await _db.Reviews.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Review>> GetReview(int id)
+    {
+      Review review = await _db.Reviews.FindAsync(id);
+
+      if (review == null)
+      {
+        return NotFound();
+      }
+
+      return review;
     }
   }
 }
